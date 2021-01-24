2fc52977ecfc1e253da149e6ff5ac492ec904472
Jonny Reading <jonnyreading@gmail.com>
2021-01-24 15:04:47 +0000

    get world data from db

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import { useState, useEffect } from 'react';
 import fire from "./components/firebase/firebase";
-import { Button } from "./components/ui/UIElements";
+import Settings from "./components/settings/Settings";
+import { Button, Slider } from "./components/ui/UIElements";
 import Page from "./components/page/Page";
 import './App.css';
 
 function App() {
 
   const [view, setView] = useState("uk");
-  const [vars, setVars] = useState({});
+  const [ukVars, setUKVars] = useState({});
   const [ukData, setUKData] = useState({});
+  const [worldVars, setWorldVars] = useState({});
+  const [worldData, setWorldData] = useState({});
 
   useEffect(() => {
-    fire.database().ref("vars").on("value", snapshot => {
-      setVars(snapshot.val())
+    fire.database().ref("uk").on("value", snapshot => {
+      setUKData(snapshot.val().data);
+      setUKVars(snapshot.val().vars);
     });
-    fire.database().ref("data").on("value", snapshot => {
-      setUKData(snapshot);
+    fire.database().ref("world").on("value", snapshot => {
+      setWorldData(snapshot.val().data);
+      setWorldVars(snapshot.val().vars);
     });
+    // fire.database().ref("data").on("value", snapshot => {
+    //   setUKData(snapshot);
+    // });
   }, []);
 
   return (
@@ -26,12 +34,10 @@ function App() {
         <Button click={() => setView("vaccines")} text="Vaccines" view={view} size="large" />
         <Button click={() => setView("world")} text="World" view={view} size="large" />
       </header>
-      {view === "uk" && (
-        <Page view={view} vars={vars} data={ukData} />
-      )}
-      {view === "world" && (
-        <Page view={view} vars={vars} data={{}} />
-      )}
+      {view === "uk" && <Page view={view} data={ukData} />}
+      {/* {view === "vaccines" && <Page view={view} data={{}} />} */}
+      {/* {view === "world" && <Page view={view} data={{}} />} */}
+      <Settings />
     </div>
   );
 }
